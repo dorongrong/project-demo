8911192aff9937bde9ca8079c03a6667b0ed903d
dorongrong <84131419+dorongrong@users.noreply.github.com>
2023-09-05 17:48:26 +0900

    init

---
+++ src/test/java/lee/projectdemo/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.projectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
